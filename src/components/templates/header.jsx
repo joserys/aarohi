cbc26465544f7a7a0b76b25c79f3fc834d7cbb17
aarsri <43250555+aarsri@users.noreply.github.com>
2025-03-26 21:34:39 -0400

    Update header.jsx

--- src/components/templates/header.jsx
+++ src/components/templates/header.jsx
@@ -16,7 +16,7 @@ const Header = ({hide = false}) => {
 				</div>
 			</div>
 			<Url icon="fa-solid fa-location-dot" link="https://www.nd.edu/" external={true}>University of Notre Dame</Url>
-			<Url icon="fa-solid fa-school" link="https://nlp.nd.edu/" external={true}>Natural Language Processing</Url>
+			<Url icon="fa-brands fa-google-scholar" link="https://scholar.google.com/citations?user=Mv6HFkAAAAAJ&hl=en&oi=ao/" external={true}>Google Scholar</Url>
 			<Url icon="fa-solid fa-envelope" link="mailto:asrivas2@nd.edu">Email</Url>
 			<Url icon="fa-brands fa-linkedin" link="https://www.linkedin.com/in/aarohi-srivastava" external={true}>LinkedIn</Url>
 			<Url icon="fa-brands fa-github" link="https://github.com/aarsri" external={true}>Github</Url>
