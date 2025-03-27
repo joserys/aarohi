f9fa7c848917bb1706a884cfe107b8fbbc2bab66
joserys <jose.rys@outlook.com>
2025-03-26 20:02:14 -0400

    final updates

--- src/components/templates/footer.jsx
+++ src/components/templates/footer.jsx
@@ -3,7 +3,7 @@ import Text from "../modules/text";
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<Text type="normal" classOverride="center">© 2025 Aarohi Srivastava & Jose Lopez - Reyes</Text>
+			<Text type="normal" classOverride="center">© 2025 Aarohi Srivastava & Jose Lopez-Reyes</Text>
 		</footer>
     );
 }
